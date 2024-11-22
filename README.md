a1fc8cac9252b14a97c30be1222323795de275af
krystellar <krystel.zerep@gmail.com>
2024-11-22 11:32:37 +0800

    added faci and services

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # UP-survival-kit
 Cmsc 3 final requirement
+
+[] general
+    <x> navigation bar
+    <> logo
+    <> footer
+
+[] homepage
+    <x> up quality policy
+    <x> news and announcement
+    <> up mindanao history (optional)
+    <> up naming mahal
+    <> emails to remember
+
+[] student life
+    <X> culture and community
+    <X> clubs and organizations
+    <x> interactive map
+    <x> facilities
+    <x> OSA services
+    <> student's survival tip
+    <> grade calculator (!!IMPORTANT!!)
--- script.js
+++ script.js
@@ -19,12 +19,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     const mapAreas = document.querySelectorAll('.map-area');
+    const locationTitle = document.getElementById('location-title');
     const locationDescription = document.getElementById('location-description');
+    const locationImage = document.getElementById('location-image');
     
     mapAreas.forEach(area => {
         area.addEventListener('mouseenter', function() {
+            const title = area.getAttribute('data-title');
             const description = area.getAttribute('data-description');
+            const image = area.getAttribute('data-image');
+            locationTitle.textContent = title;
             locationDescription.textContent = description;
+            locationImage.src = image;
         });
     });
 });
